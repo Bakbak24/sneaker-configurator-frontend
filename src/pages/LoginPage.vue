ca235c1362ad6076915661e6af4ae3b3dab2e9ca
Bakbak24 <zakabak3@gmail.com>
2024-11-16 21:53:12 +0100

    feat: implement login page and update routing

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Login Page</h1>
+    <form @submit.prevent="handleLogin">
+      <input v-model="username" placeholder="Username" />
+      <input type="password" v-model="password" placeholder="Password" />
+      <button type="submit">Login</button>
+    </form>
+    <p v-if="errorMessage">{{ errorMessage }}</p>
+  </div>
+</template>
+
+<script>
+</script>
